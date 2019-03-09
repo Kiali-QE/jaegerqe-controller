f304bc1076213350db1d49e9197541e0c793940d
Jeeva kandasamy <jkandasa@redhat.com>
2019-03-09 12:24:54 +0530

    added try catch on mqtt message handling

--- src/main/java/io/jaegertracing/qe/controller/mqtt/SimpleCallback.java
+++ src/main/java/io/jaegertracing/qe/controller/mqtt/SimpleCallback.java
@@ -15,35 +15,42 @@ import lombok.extern.slf4j.Slf4j;
 public class SimpleCallback implements MqttCallback {
 
     public void connectionLost(Throwable throwable) {
-        logger.error("Connection lost to the broker [{}], wait 10 seconds and reconnect",
-                MqttUtils.config().getHostUrl());
         try {
+            logger.error("Connection lost to the broker [{}], wait 10 seconds and reconnect",
+                    MqttUtils.config().getHostUrl());
             Thread.sleep(1000 * 10);
-        } catch (InterruptedException ex) {
+            MqttUtils.connect();
+        } catch (Exception ex) {
             logger.error("Exception,", ex);
         }
-        MqttUtils.connect();
     }
 
     @SuppressWarnings("unchecked")
     public void messageArrived(String topic, MqttMessage message) {
-        logger.info("Message received: [topic:{}, payloadLength:{}, qos:{}]",
-                topic, message.getPayload().length, message.getQos());
-        if (topic.startsWith(MqttUtils.TOPIC_ABOUT_REPORTER)) {
-            Map<String, Object> data = (Map<String, Object>) SerializationUtils.deserialize(message.getPayload());
-            ReporterNodeRepo.updateReporters(data);
-        } else {
-            logger.info("No action performed");
+        try {
+            logger.info("Message received: [topic:{}, qos:{}, payloadLength:{}]",
+                    topic, message.getQos(), message.getPayload().length);
+            if (topic.startsWith(MqttUtils.TOPIC_ABOUT_REPORTER)) {
+                Map<String, Object> data = (Map<String, Object>) SerializationUtils.deserialize(message.getPayload());
+                ReporterNodeRepo.updateReporters(data);
+            } else {
+                logger.info("No action performed");
+            }
+        } catch (Exception ex) {
+            logger.error("Exception,", ex);
         }
-
     }
 
     public void deliveryComplete(IMqttDeliveryToken token) {
-        StringBuilder topics = new StringBuilder();
-        for (String topic : token.getTopics()) {
-            topics.append(topic).append(", ");
+        try {
+            StringBuilder topics = new StringBuilder();
+            for (String topic : token.getTopics()) {
+                topics.append(topic).append(", ");
+            }
+            logger.debug("delivery complete. [topics:{}]", topics);
+        } catch (Exception ex) {
+            logger.error("Exception,", ex);
         }
-        logger.debug("delivery complete. [topics:{}]", topics);
     }
 
 }
